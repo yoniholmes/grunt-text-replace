b77e9b87e00567eaedfc0752637f7792f525d16b
Jonathan Holmes <yoni.holmes@gmail.com>
2012-11-21 10:18:04 +0100

    Tests coming along, now need to capture error states, then done.

--- tasks/grunt-text-replace.js
+++ tasks/grunt-text-replace.js
@@ -12,6 +12,24 @@ module.exports = function (_grunt) {
 
 
 plugin = {
+  errorMessages: {
+    noTargets: "No targets were found. Remember to wrap functionality within " +
+      "a target.",
+    noSourceFiles: "No source files found",
+    noDestination: "Destination is not defined! If you want to overwrite " +
+      "files, then make sure to set overwrite: true. If you don't wish to " +
+      "overwrite, then make sure to set a destination",
+    noReplacements: "No replacements were found.",
+    overwriteFailure: "Overwrite is to true, but a destination has also " +
+      "been defined. If you want to overwrite files, remove the destination. " +
+      "If you want to send files to a destination, then ensure overwrite is " +
+      "not set to true",
+    multipleSourceSingleDestination: "Cannot write multiple files to same " +
+      "file. If you wish to export to a directory, make sure there is a " + 
+      "trailing slash on the destination. If you wish to write to a single " +
+      "file, make sure there is only one source file"
+  },
+
   initialisePlugin: function () {
     this.registerTasks();
     this.registerHelpers();
@@ -25,75 +43,81 @@ plugin = {
     this.registerTextReplaceHelper();
     this.registerTextReplaceMultipleHelper();
     this.registerTextReplaceFileHelper();
+    this.registerTextReplaceFileMultipleHelper();
+    this.registerReplaceHelper();
   },
 
   registerTextReplaceTask: function () {
-    grunt.registerMultiTask('replace', 'Description', this.replace);
+    grunt.registerMultiTask('replace', 'Description', this.replaceTask);
   },
 
   registerTextReplaceHelper: function () {
-    grunt.registerHelper('text-replace', this.textReplace);
+    grunt.registerHelper('text-replace', this.textReplaceHelper);
   },
 
   registerTextReplaceMultipleHelper: function () {
-    grunt.registerHelper('text-replace-multiple', this.textReplaceMultiple);
+    grunt.registerHelper('text-replace-multiple', this.textReplaceMultipleHelper);
   },
 
   registerTextReplaceFileHelper: function () {
-    grunt.registerHelper('text-replace-file', this.textReplaceFile);
+    grunt.registerHelper('text-replace-file', this.textReplaceFileHelper);
   },
 
-  replace: function () {
+  registerTextReplaceFileMultipleHelper: function () {
+    grunt.registerHelper('text-replace-file-multiple', this.textReplaceFileMultipleHelper);
+  },
+
+  registerReplaceHelper: function () {
+    grunt.registerHelper('replace', this.replaceHelper);
+  },
+
+  replaceTask: function () {
     var src = this.data.src;
     var dest = this.data.dest;
     var replacements = this.data.replacements;
-    
-// var finalDestination;
-// var fileName = path.basename(dest);
-// if (this.data.overwrite) {
-  
-// }
-    
-
-
-// getWriteToPathForFile: function (pathToFile) {
-//   var fileName = path.basename(pathToFile), 
-//     destination;
-//   if (this.isOverwriteTrue) {
-//     destination = pathToFile;
-//   } else {
-//     destination = this.pathToDestination + 
-//       (this.isDestinationDirectory ? fileName : '');
-//   }
-//   return destination;
-// },    
-
-
-
-
     grunt.helper('text-replace-file', src, dest, replacements);
   },
 
-  textReplace: function (fullText, pattern, replacement) {
+  textReplaceHelper: function (fullText, pattern, replacement) {
     var regex = plugin.convertToRegex(pattern);
     var treatedReplacement = plugin.treatReplacement(replacement);
     return fullText.replace(regex, treatedReplacement);
   },
 
-  textReplaceMultiple: function (fullText, allReplacements) {
+  textReplaceMultipleHelper: function (fullText, allReplacements) {
     return allReplacements.reduce(function (fullText, replacement) {
       return grunt.helper('text-replace', fullText, replacement.from, replacement.to);
     }, fullText);
   },
 
-  textReplaceFile: function (src, dest, replacements) {
-    grunt.file.copy(src, dest, {
+  textReplaceFileHelper: function (pathToSource, dest, replacements) {
+    var isDestinationDirectory = (/\/$/).test(dest);
+    var fileName = path.basename(pathToSource);
+    var pathToDestination = dest + (isDestinationDirectory ? fileName : '');
+    grunt.file.copy(pathToSource, pathToDestination, {
       process: function (fullText) {
         return grunt.helper('text-replace-multiple', fullText, replacements);
       }
+    });  
+  },
+
+  textReplaceFileMultipleHelper: function (src, dest, replacements) {
+    var sourceFiles = grunt.file.expandFiles(src);
+    sourceFiles.forEach(function (pathToSource) {
+      grunt.helper('text-replace-file', pathToSource, dest, replacements);
     });
   },
 
+  replaceHelper: function (settings) {
+    var src = settings.src;
+    var dest = settings.dest;
+    var overwrite = settings.overwrite;
+    var replacements = settings.replacements;
+
+    if (typeof src === 'undefined' || src.length === 0) {
+      grunt.warn(plugin.errorMessages.noSourceFiles);
+    }
+  },
   
   // TO DO: rename this to something more useful
   convertToRegex: function (pattern) {
