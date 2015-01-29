5f7fd93943ccc9a2dd8a0629eecd73f4c7da3e45
slanders <slanders@ebsco.com>
2015-01-29 17:03:05 -0600

    renamed references to grunt-text-replace

--- tasks/text-replace.js
+++ tasks/text-replace.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  */
 
-var gruntTextReplace = require('../lib/grunt-text-replace');
+var gruntTextReplace = require('../lib/grunt-match-replace');
 
 module.exports = function(grunt) {
 
